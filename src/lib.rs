9be9640875e918385701d1dc827631e0aa1bbc8a
pepeamoedo <pepeamoedo@me.com>
2026-06-03 04:19:59 +0200

    chore: use console_error_panic_hook::set_once for better panic traces

--- src/lib.rs
+++ src/lib.rs
@@ -1026,7 +1026,7 @@ impl<'a> State<'a> {
 pub async fn run() {
     #[cfg(target_arch = "wasm32")]
     {
-        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_error_panic_hook::set_once();
         console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
     }
 
